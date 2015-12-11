83c77435d6ea59c898daa3182ed80537747e2984
James Trinklein <james.trinklein@changepoint.com>
2015-12-11 12:58:50 -0800

    ignore eof warnings

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ chatbot = ChatBot("HAL 9000")
 chatbot.train("chatterbot.corpus.english")
 
 while True:
-    message = raw_input("> ")
-
-    if "exit" in message:
-        print("Well that's rude.  Goodbye")
-        exit()
-
-    chatbot.get_response(message)
-
+    try:
+        message = raw_input("> ")
+        if "exit" in message:
+            print("Well that's rude.  Goodbye")
+            exit()
+        else:
+            chatbot.get_response(message)
+    except EOFError e:
+        print("got eof")
