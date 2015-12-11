62e44b9fe9a383689105f29beb0518bf91c8fa1d
James Trinklein <james.trinklein@changepoint.com>
2015-12-11 11:38:31 -0800

    better listener

---
+++ listen.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+# NOTE: this example requires PyAudio because it uses the Microphone class
+
+import speech_recognition as sr
+
+def do_recognize(key, use_wit):
+    phrase = ""
+    engine = ""
+    if use_wit:
+        # recognize speech using Wit.ai
+        WIT_AI_KEY = "AVPNCOC732URGDUWILQ333FQLWJZBZON" # Wit.ai keys are 32-character uppercase alphanumeric strings
+        phrase = r.recognize_wit(audio, key=WIT_AI_KEY)
+        engine = "Wit.ai"
+
+    else:
+        # received audio data, now we'll recognize it using Google Speech Recognition
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        if key is None:
+            recognizer.recognize_google(audio)
+        else:
+            recognizer.recognize_google(audio, key)
+
+        phrase = recognizer.recognize_google(audio)
+        engine = "Google Speech Recognition"
+
+    print(engine + " thinks you said: " + phrase)
+    return phrase
+
+def read_key():
+    key = None
+    with open('./currkey', 'r') as keyfile:
+        key = keyfile.readline().strip('\n')
+
+    return key
+
+def write_key(key):
+    with open('./currkey', 'w') as keyfile:
+        keyfile.write(key)
+
+def keychange(last_key):
+    keys = None
+    with open('./keys') as file:
+        keys = [line.strip('\n') for line in file]
+
+    key_index = keys.index(last_key) if last_key in keys else None
+    next_key = keys[key_index+1] if key_index is not None and key_index < len(keys) else 'wit'
+
+    write_key(next_key)
+
+    return next_key
+
+# this is called from the background thread
+def callback(recognizer, audio):
+    key = read_key()
+
+    use_wit = key == 'wit'
+
+    retries = 3
+    while retries > 0:
+        try:
+            retries--
+            return do_recognize(key, use_wit)
+
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+            return ""
+        except sr.RequestError as e:
+            print("Could not request results from service; {0}".format(e))
+            if not use_wit:
+                print("going to try changing keys")
+                key = keychange(key)
+                if key == 'wit':
+                    print('falling back to wit')
+
+
+r = sr.Recognizer()
+m = sr.Microphone()
+with m as source:
+    r.adjust_for_ambient_noise(source) # we only need to calibrate once, before we start listening
+
+# start listening in the background (note that we don't have to do this inside a `with` statement)
+stop_listening = r.listen_in_background(m, callback)
+# `stop_listening` is now a function that, when called, stops background listening
+
+# do some other computation for 5 seconds, then stop listening and keep doing other computations
+import time
+running = True
+getinput = raw_input or input
+while running:
+    cmd = getinput().lower()
+    if (cmd == 'quit' or cmd == 'exit'):
+        print('exiting')
+        stop_listening()
+        running = False
--- readme.md
+++ readme.md
@@ -1,25 +1,7 @@
-Marvin
+Robot
 ---------
 
-[![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/foresterh/marvin?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)  (opens in same tab)
-
-Python bot for fun and usefulness.
-
-Got it's attitude from the Hitchhiker's Guide to the Galaxy
-
-Got it's roots from pyFlowbot and Phenny and a few other distant cousins.
-
-Now it's own bot.
-
-Works (mostly) with python 3.4.  No guarantee for earlier (or 2.*) versions
-I recommend using [pyenv](https://github.com/yyuu/pyenv) to run separate versions of python concurrently.
-
--To use
-(optionally) install and run redis (in separate tab)
-
-``` $ brew install redis ```
-
-``` $ redis-server ```
+It's a bot
 
 install pip
 
@@ -27,23 +9,5 @@ install pip
 
 install requirements
 
-``` $ sudo pip install -r requirements.txt ```
+``` sudo pip install -r requirements.txt ```
 
-copy core/config.copy to ./config and update logins and passwords (to use flowdock and any authenticated apis like github)
-
-    $ cp core/config.copy config
-
-mark bot.py as executable or just run with python:
-
-    $ python bot.py
-
-add argument to determine adapter (where input and output come from).  Currently only accepts flowbot (flowdock) and console
-
-    $ python bot.py -a console
-
-when all else fails
-     
-``` $ python bot.py -h ```
-
---Storage
-Storage module uses redis, so unless you change that, you should have redis running on the default settings.
