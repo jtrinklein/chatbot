3eab380925e61d3d6a7028cad2911bcfba9a2889
James Trinklein <james.trinklein@changepoint.com>
2015-12-11 17:11:39 -0500

    more say stuff

--- say.py
+++ say.py
@@ -7,5 +7,6 @@ while True:
     with open('/home/ubuntu/sayio', 'r') as input:
         message = input.read()
         if message:
+            print('> ' + message)
             call('espeak', ['-ven+m2','-k5', '-s150', '"' + message + '"'])
 
