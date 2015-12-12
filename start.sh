0830eda56b62d487da2bf0e72f1619f01698571c
James Trinklein <james.trinklein@changepoint.com>
2015-12-11 20:25:12 -0500

    startup script

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+screen -dmS "chatbot" python ./main.py
+screen -dmS "speech" python ./say.py
+screen -dmS "listen" python ./listen.py
+screen -dmS "eyes" ~/Desktop/pi-facerec-box-master/start.sh
+
