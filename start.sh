f88a38d254774dfe1fb6d161fcdce7dceb88de47
James Trinklein <james.trinklein@changepoint.com>
2015-12-16 12:28:32 -0500

    disable vision

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 screen -dmS "chatbot" python ./main.py
 screen -dmS "speech" python ./say.py
 screen -dmS "listen" python ./listen.py
-screen -dmS "eyes" ~/Desktop/pi-facerec-box-master/start.sh
+#screen -dmS "eyes" ~/Desktop/pi-facerec-box-master/start.sh
 
